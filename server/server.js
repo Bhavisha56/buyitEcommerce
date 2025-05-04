0e9e5d06f3bbc02be5929927a57f1332c2c0524a
Bhavisha56 <97666630+Bhavisha56@users.noreply.github.com>
2025-05-04 16:44:10 +0530

    change in cors

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(cookieParser())
 
 
 const corsOptions = {
-   origin: "http://localhost:5173", 
+   origin: "https://buyitecommerce.onrender.com", 
    methods: "GET,POST,PUT,PATCH,DELETE,HEAD",
    credentials: true,
  };
